6a471642f374119c674b541dbef4f7419b596aa6
DASARI ABHILASH <dasariabhi014@gmail.com>
2026-01-29 16:19:31 +0530

    Initial commit - complete Todo List project

---
+++ backend/server.js
@@ -0,0 +1,71 @@
+const express=require('express');
+const cors=require('cors');
+const mysql=require('mysql2');
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root@2005',
+    database:'todo'
+})
+db.connect((error) =>{
+    if(error){
+        console.log('Error is getting in connection of database mysql')
+    return
+    }
+    console.log('Mysql is connected')
+})
+app.get('/',(request,response) =>{
+    console.log('Default routes')
+    db.query(`select * from todoItems`, (error,result) =>{
+        if(error){
+            console.log('error occured',error)
+            return
+        }
+       console.log('Data: ',result)
+       response.send(result)
+    })
+})
+app.delete('/delete-item/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM todoItems WHERE ID =${ID} ', (error, result) => {
+        if (error) {
+            console.log('Delete error', error);
+            return res.status(500).send('Error deleting item');
+        }
+        res.send('deleted');
+    });
+});
+
+
+app.post('/add-item',(request,response) =>{
+    console.log(request.body)
+    db.query(`insert into todoItems(itemDescription) values('${request.body.text}')`, (error, result) => {
+        if(error){
+            console.log('error occured')
+            return
+        }
+       console.log('Created Successfully')
+    
+    })
+    response.send('added-successfully')
+})
+app.put('/edit-item', (request, response) => {
+    console.log('Line 54 ',request.body);
+     db.query(`update todoitems set itemDescription='${request.body.itemDescription}' where ID=${request.body.ID};`, (error, result) => {
+          if (error) {
+        console.log("Error occured", error)
+        return
+      }
+      console.log("Updated Successfully")
+    }
+  )
+  response.send('success')
+})
+
+app.listen(3000,()=> {
+    console.log('server is running at 3000 port successfully')
+})
